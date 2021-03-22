99609d94b5856cede0ccaec3f8bd748e74ccd55a
sidraj000 <siddharthraj000@gmail.com>
2021-03-22 15:31:54 +0530

    dburi added

--- app/__init__.py
+++ app/__init__.py
@@ -11,12 +11,12 @@ def create_app():
     app.config['CORS_HEADERS'] = 'Content-Type'
     CORS(app)
     #for production
-    # app.config.from_object(os.environ['APP_SETTINGS'])
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
     # for local
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:8529454669@localhost:5432/ugproject"
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:8529454669@localhost:5432/ugproject"
     
     db.init_app(app)
     migrate = Migrate(app, db)
