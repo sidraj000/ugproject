42d47762b6e885ec9f7b314dd61d6256d3b4b20d
sidraj000 <siddharthraj000@gmail.com>
2021-03-23 00:09:38 +0530

    fixed

--- project/__init__.py
+++ project/__init__.py
@@ -10,9 +10,9 @@ import os
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:8529454669@localhost:5432/ugproject"
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-    print(os.environ['DATABASE_URL'])
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:8529454669@localhost:5432/ugproject"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+  
     db.init_app(app)
 
     app.register_blueprint(savings)
