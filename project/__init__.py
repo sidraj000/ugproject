4c34293aa8229333db91d2da7ea934857df4b450
sidraj000 <siddharthraj000@gmail.com>
2021-04-21 10:25:10 +0530

    changed uri

--- project/__init__.py
+++ project/__init__.py
@@ -9,8 +9,9 @@ import os
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://pmcollpzcljpds:aa7b1541d96c727d366e14ad88c10d3cacea61dc362e6eefb2c5a912a15022ba@ec2-54-211-176-156.compute-1.amazonaws.com:5432/d4nv92cbpqbodp"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://fddbhhljcdbkqu:169404ddc1e49eb071296be4315adac3ac680163d6386a778ca39e877b31cf85@ec2-52-0-114-209.compute-1.amazonaws.com:5432/d8hpdqra6gebcc"
     # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:8529454669@localhost:5432/ugproject"
+    # postgres://fddbhhljcdbkqu:169404ddc1e49eb071296be4315adac3ac680163d6386a778ca39e877b31cf85@ec2-52-0-114-209.compute-1.amazonaws.com:5432/d8hpdqra6gebcc
    
     db.init_app(app)
 
